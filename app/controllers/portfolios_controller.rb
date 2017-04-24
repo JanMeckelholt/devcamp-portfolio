2e920dc183a1a42e678ab1adcb5f68e0fbf5dfc6
JanMeckelholt <Jan_Meckelholt@web.de>
2017-04-24 19:20:03 +0200

    new and create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,36 @@
 class PortfoliosController < ApplicationController
+before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
 
 def index
   @portfolio_items = Portfolio.all
 end
 
+def new
+  @portfolio_item = Portfolio.new
+end
+
+def create
+    @portfolio_item = Portfolio.new(blog_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else  
+        format.html { render :new }
+        
+      end
+    end
+  end
+
+private
+
+def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+ def blog_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 
 end
