f3ba3d3b409db48018bd3f78fd80171a217b0f20
JanMeckelholt <Jan_Meckelholt@web.de>
2017-05-13 16:28:24 +0200

    styles for Portfolio header an abstract

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,16 @@
 module ApplicationHelper
 
-  def login_helper(style='')
+  def login_helper(style1='', style2='')
     
-
+    if style2 == ''
+      style2 = style1
+    end
+    
     unless current_user.is_a?(GuestUser)
-     link_to "Logout", destroy_user_session_path, method: :delete, class: style 
+     link_to "Logout", destroy_user_session_path, method: :delete, class: style1 
      else 
-      (link_to "Register", new_user_registration_path, class: style) + 
-      (link_to "Login", new_user_session_path, class: style) 
+      (link_to "Register", new_user_registration_path, class: style1) + 
+      (link_to "Login", new_user_session_path, class: style2) 
     end 
   end
   
